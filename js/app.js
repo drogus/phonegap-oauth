5ca42d6ecc8824110a7cf2dee3afa010f155ee82
alunny <alunny@gmail.com>
2011-09-01 15:31:07 -0700

    cross-platform javascripts

--- js/app.js
+++ js/app.js
@@ -1,33 +1,7 @@
-console.log(ChildBrowser);
-console.log('works?');
+document.addEventListener('deviceready', function () {
+    if (typeof ChildBrowser.install == "function") {
+        ChildBrowser.install();
+    }
 
-PhoneGap.addConstructor(function() {
-  PhoneGap.addPlugin("childBrowser", new ChildBrowser());
-  PluginManager.addService("ChildBrowser", "com.phonegap.plugins.childBrowser.ChildBrowser");
-});
-
-PhoneGap.exec(null, null, "ChildBrowser", "showWebPage", ["http://www.google.com", false]);
-PhoneGap.exec(null, null, "com.phonegap.plugins.childBrowser.ChildBrowser", "showWebPage", ["http://www.google.com", false]);
-
-//var childBrowser = new ChildBrowser();
-//childBrowser.showWebPage("http://www.google.com");
-// var client_browser = ChildBrowser.install();
-// client_browser.onLocationChange = function(loc){
-//   jQuery("#messages").append("<p>child browser location change" + loc + "</p>");
-//   client_browser.close();
-// };
-
-// console.log(client_browser);
-
-// if (client_browser != null) {
-//   window.plugins.childBrowser.showWebPage("/browser.html");
-// }
-
-
-
-
-
-
-
-
-
+    window.plugins.childBrowser.showWebPage("http://www.google.com");
+}, false);
