1e1d8ce4b6f1e3b0b885e260d74c1a8d109c40f6
alunny <alunny@gmail.com>
2011-09-01 15:37:35 -0700

    kinda sorta unified api

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 document.addEventListener('deviceready', function () {
     if (typeof ChildBrowser.install == "function") {
         ChildBrowser.install();
+    } else {
+        window.plugins = window.plugins || {};
+        window.plugins.childBrowser = new ChildBrowser;
     }
 
     window.plugins.childBrowser.showWebPage("http://www.google.com");
